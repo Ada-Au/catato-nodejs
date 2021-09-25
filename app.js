c4631e4b684bf6543f7b20a14ca1e63d8640ba3f
Ada <adaau0209@gmail.com>
2021-09-25 13:23:01 +0800

    allowing routing yesssss

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const morganBody = require("morgan-body");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express().use(express.json());
 morganBody(app, { noColors: process.env.NODE_ENV === "production" });
 
-// require("./routes/asteroid")(app);
-var asteroid = require("./routes/asteroid");
-app.post("/asteroid", asteroid(req, res));
+require("./routes/asteroid")(app);
+// var asteroid = require("./routes/asteroid");
+// app.post("/asteroid", (req, res) => {
+//   console.log("test");
+//   console.log(req);
+//   asteroid(req, res);
+// });
 
 app
   .post("/square", (req, res) => {

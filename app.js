c0acd802e91be368ec13649efff2e109604e8506
Ada <adaau0209@gmail.com>
2021-09-25 11:38:02 +0800

    add homepage for testing

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const morganBody = require("morgan-body");
+const express = require('express');
+const morganBody = require('morgan-body');
 const PORT = 5000;
 
 const app = express().use(express.json());
-morganBody(app, { noColors: process.env.NODE_ENV === "production" });
+morganBody(app, { noColors: process.env.NODE_ENV === 'production' });
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
 app
-  .post("/square", (req, res) => {
+  .post('/square', (req, res) => {
     const output = parseInt(req.body.input) ** 2;
     res.json(output);
   })

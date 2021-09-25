d45da87b185cb71d15ed73e3c2bc6b4e827043a5
Ada <adaau0209@gmail.com>
2021-09-25 10:26:40 +0800

    update app setting

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const morganBody = require('morgan-body');
-const PORT = process.env.PORT || 5000;
+const express = require("express");
+const morganBody = require("morgan-body");
+const PORT = 5000;
 
 const app = express().use(express.json());
-morganBody(app, { noColors: process.env.NODE_ENV === 'production' });
+morganBody(app, { noColors: process.env.NODE_ENV === "production" });
 
 app
-  .post("/square", (req, res) => {
+  .get("/square", (req, res) => {
     const output = parseInt(req.body.input) ** 2;
     res.json(output);
   })
